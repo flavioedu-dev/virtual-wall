8744eb100f2102307312b9af76ae17d53702007b
Flávio Eduardo <xflax00@gmail.com>
2023-08-31 16:09:01 -0300

    feat: add nav component

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,10 +1,17 @@
-import Navbar from '@/components/Nav/Navbar'
-import React from 'react'
+import Navbar from "@/components/Nav/Navbar";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <Navbar></Navbar>
-  )
-}
+    <>
+      <Navbar></Navbar>
+      <main>
+        <div>
+          <h1>Murais</h1>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
+"use client";
 
+// CSS
 import styles from "./navbar.module.css"
 
+// Images
 import menuImg from "public/menu.png";
 import boardLogoImg from "public/board-logo.png";
-
 import Image from 'next/image'
+import Menu from '../Menu/Menu';
+
+// Hooks and types
+import React, { useState } from 'react'
 
 const Nav = () => {
+
+  const [stateMenu, setStateMenu] = useState<boolean>(false);
+
+  const openMenu = () => {
+    setStateMenu(true);
+  }
+
+  const closeMenu = () => {
+    setStateMenu(false);
+  }
+
   return (
-    <nav className={styles.nav_container}>
+    <nav className={styles.nav_container} >
       <Image
         src={menuImg}
         alt="menu-ico"
         style={{objectFit: "contain"}}
+        onClick={openMenu}
       />
 
       <Image
         src={boardLogoImg}
         alt="board-logo"
       />
+
+      {stateMenu && <Menu closeMenu={closeMenu} />}
     </nav>
   )
 }
