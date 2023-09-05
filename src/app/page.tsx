90324d393a0b76d44ed2cf19cdb7e7b7fcb2fa13
Ruan Lauro <ruanlaurocardosomonteirogato@gmail.com>
2023-09-05 18:19:47 -0300

    Exemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+//CSS
+import "./home_page.css"
+
+//IMG
+import logoImg from "public/Logo.png";
+import Image from "next/image";
+
+//Components
+import Presentation from "@/components/Home_presentation/Presentation";
+
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+        <Image
+          src={logoImg}
+          alt="Logo"
+          className="img_logo"
+          style={{ objectFit: "contain" }}
+        />
+        
+        <Presentation/>
+        
     </main>
   )
 }
---
+++ src/components/Home_presentation/Presentation.tsx
@@ -0,0 +1,22 @@
+import "./presentation.css"
+import frame from "public/frame.png"
+import Image from "next/image";
+
+const presentation = () => {
+  return (
+    <main className="all-home">
+        <div>
+            <h2>Olá, somos o Mural Virtual! Iremos melhorar sua forma de informar e de se informar!</h2>
+
+            <p>Aqui você pode cadastrar seu grupo e assim fazer com que seus integrantes entre nele.</p>
+        </div>
+        <Image 
+          src={frame}
+          alt="frame"
+          className="frame"
+          style={{ objectFit: "contain" }}/>
+    </main>
+  )
+}
+
+export default presentation
